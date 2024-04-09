27d7f9a19543fb0fdacfe666650be090c2e4ae3d
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-09 23:40:15 +0000

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+"""A simple flask app Module"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """configuration for language"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def hello() -> str:
+    """outputs welcome message"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
