0d414ef030ecb622a9429ca91964c7d6a6193e1b
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-10 01:00:01 +0100

    Updated the babel language config

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -7,20 +7,20 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
-# class Config:
-#    """configuration for language"""
-#    LANGUAGES = ["en", "fr"]
-#    BABEL_DEFAULT_LOCALE = "en"
-#    BABEL_DEFAULT_TIMEZONE = "UTC"
+class Config:
+    """configuration for language"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# app.config.from_object(Config)
+app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
     """determines best match from supported languages"""
-    return request.accept_languages.best_match(["en", "fr"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
