0936caf2f04b7e49500b9ab193b4b734426df76d
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-10 00:29:03 +0000

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+"""A simple flask app Module"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """configuration for language"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """determines best match from supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def hello() -> str:
+    """outputs welcome message"""
+    return render_template(
+        '3-index.html',
+        title=_("home_title"),
+        header=_("home_header")
+    )
+
+
+if __name__ == "__main__":
+    app.run()
