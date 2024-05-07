da1b4b7c6d6838c8a999b2ceadfd483c5e0cd897
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-05-07 21:44:52 +0000

    task 0-1

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+import {createClient} from 'redis';
+
+const client = createClient()
+	.on('error', err => console.error('Redis client not connected to the server:', err))
+	.on('connect', () => console.log('Redis client connected to the server'));
